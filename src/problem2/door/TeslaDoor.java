61a15a5f03b5d68c4fe8f5c57f94f992cd5658ed
minjun <ijmtylhtf@naver.com>
2023-11-20 19:59:27 +0900

    #2 - add : Tesla 부품 구체 클래스 구현

---
+++ src/problem2/door/TeslaDoor.java
@@ -0,0 +1,8 @@
+package problem2.door;
+
+import problem2.CompanyName;
+
+public class TeslaDoor extends Door{
+
+    public TeslaDoor() { super(CompanyName.TESLA); }
+}
---
+++ src/problem2/wheel/TeslaWheel.java
@@ -0,0 +1,8 @@
+package problem2.wheel;
+
+import problem2.CompanyName;
+
+public class TeslaWheel extends Wheel{
+
+    public TeslaWheel(CompanyName factoryName) { super(CompanyName.TESLA);}
+}
