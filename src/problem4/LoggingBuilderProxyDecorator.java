013e185e9478bfd8a1ac3a34815506113b97afb5
minjun <ijmtylhtf@naver.com>
2023-11-21 15:20:39 +0900

    #4 - feat : SimpleLogger 생성자 주입

--- src/problem4/LoggingBuilderProxyDecorator.java
+++ src/problem4/LoggingBuilderProxyDecorator.java
@@ -12,12 +12,17 @@ import problem3.Car;
  * If necessary, you can make your own methods or classes so that the corresponding tests are passed
  */
 public class LoggingBuilderProxyDecorator extends BaseBuilderProxyDecorator {
+
+    private SimpleLogger logger;
     public LoggingBuilderProxyDecorator(Builder builder, SimpleLogger logger) {
+
         super(builder);
+        this.logger = logger;
     }
 
     public LoggingBuilderProxyDecorator(CarPartsFactory factory, SimpleLogger logger) {
         super(factory);
+        this.logger = logger;
     }
 
     @Override
