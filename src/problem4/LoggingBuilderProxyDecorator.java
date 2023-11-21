2461c362d66af26234af7f43b5b462e714928592
minjun <ijmtylhtf@naver.com>
2023-11-21 15:25:20 +0900

    #4 - feat : Logger Proxy에서의 getCar 구현

--- src/problem4/LoggingBuilderProxyDecorator.java
+++ src/problem4/LoggingBuilderProxyDecorator.java
@@ -1,5 +1,6 @@
 package problem4;
 
+import problem1.LogLevel;
 import problem1.SimpleLogger;
 import problem2.factory.CarPartsFactory;
 import problem3.Builder;
@@ -27,7 +28,8 @@ public class LoggingBuilderProxyDecorator extends BaseBuilderProxyDecorator {
 
     @Override
     public Car getCar() {
-        return null;
+        logger.log(LogLevel.INFO, "Building a car is completed!");
+        return builder.getCar();
     }
 }
 /**
